b96f7ea790b21b44960403cdff18b1b40464d14f
qiaomein <jackqiao2002@gmail.com>
2023-08-11 20:52:04 -0500

    refer to last commit lol

--- main/main.ino
+++ main/main.ino
@@ -1,69 +1,59 @@
 
-const int motor2pin = 9;  // not on pins 5,6
+// constants
+const int motor2pin = 9;
 const int motor4pin = 10;
-
 const int samplefreq = 250;
 const int pwmfreq = 600;
-
-// tach pins are interrupts
-const int tachpin2 = 2;
-const int tachpin4 = 3;
-
-
-const int timeout = 1;  // in s before omega is 0
+const int tachpin2 = 2; //interruptpin
+const int tachpin4 = 3; // interruptpin
+const int timeout = 1;  // seconds before rpm = 0
 const float pi = PI;
-
 const int startuppwm = 31936;
-int refpwm = startuppwm+6; ///////////////////////////////////////////////////////////////////////////////////////////////
 
-int pwm2, pwmphase;
+// declarations
+int refpwm = startuppwm+6;
+
+int pwm2, pwmphasereading, tpwm;
 float error, cumerror, errorp, cumerrorp, derror, preverror;
+unsigned long t, tprior, dt; // for tracking loop time
 
-float kp = .0015;
-float ki = .00025;
-float kd = .00000;
-
-float kpp = 10;
-float kip = .002;
-
-
-int reading, tpwm;
-unsigned long t, tprior, dt;
-
-
-// tachometer variables
+// volatile variables
 volatile unsigned long dt2, d02, dt24, d024, time_shift;
 volatile float rpm, rpm4;
 
 volatile int reading2, reading4;
-
 volatile float phase, phase4;
 
 
+// CONTROLLER GAINS BELOW
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Speed control
+float kp = .0015;
+float ki = .00025;
+float kd = .00000;
+
+// Phase control
+float kpp = 10;
+float kip = .002;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void setup() {
-
-  // put your setup code here, to run once:
-  //pinMode(tachpin, INPUT);
-
 
   attachInterrupt(digitalPinToInterrupt(tachpin2), tach, RISING);
   attachInterrupt(digitalPinToInterrupt(tachpin4), tach4, RISING);
 
-  drive(motor2pin,startuppwm, pwmfreq);
-  delay(3000);
-  drive(motor4pin,startuppwm, pwmfreq);
-  delay(3000);
+  startMotors();
 
   Serial.begin(115200);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while (Serial.available() > 0) {
-    // look for the next valid integer in the incoming serial stream:
-    int inputpwm = Serial.parseInt();
 
-    // look for the newline. That's the end of your sentence:
+  // serial input sets the reference pwm to power the reference motor
+  while (Serial.available() > 0)
+  {
+    int inputpwm = Serial.parseInt();
     if (Serial.read() == '\n') {
       refpwm = inputpwm;
     }
@@ -73,9 +63,8 @@ void loop() {
   t = micros();
 
 
-  if ((t - tprior) >= 1e6 / samplefreq) {
-    //analogWrite(motor2pin, refpwm);  // drive the reference motor
-    drive(motor2pin, refpwm, pwmfreq);
+  if ((t - tprior) >= 1e6 / samplefreq) { // set constant sampling time for the controller
+    drive(motor2pin, refpwm, pwmfreq); // drive the reference motor
 
     if ((micros() - d02) > timeout * 1e6) {
       rpm = 0;
@@ -84,7 +73,6 @@ void loop() {
       rpm4 = 0;
     }
 
-    //error = phase - phase4;
     error = rpm - rpm4;
     cumerror = cumerror + error;
     derror = error - preverror;
@@ -94,30 +82,26 @@ void loop() {
 
     errorp = phase - phase4;
     cumerrorp = cumerrorp + errorp;
-
     pwmphase = kp * errorp + cumerrorp * ki;
 
     pwm2 = constrain(pwm2, startuppwm, startuppwm + 60);
-    drive(motor4pin, pwm2, pwmfreq);
+    drive(motor4pin, pwm2, pwmfreq); // drive the following motor
 
 
     dt = t - tprior;
+
     phase = phase + (dt * 1e-6 * rpm) / 60.0 * 360.0;
     phase = fmod(phase, 360.0);
     phase4 = phase4 + (dt * 1e-6 * rpm4) / 60.0 * 360.0;
     phase4 = fmod(phase4, 360.0);
 
 
-    
-    
     Serial.print(t);
     Serial.print(",");
-
     Serial.print(digitalRead(tachpin2));
     Serial.print(',');
     Serial.print(digitalRead(tachpin4));
     Serial.print(',');
-
     Serial.print(pwm2);
     Serial.print(",");
     Serial.print(1.0e6 / dt);
@@ -125,10 +109,6 @@ void loop() {
     Serial.print(error);
     Serial.print(',');    
     Serial.println(errorp);
-    
-    
-    
-
     // phases are in degrees
 
 
@@ -154,28 +134,26 @@ void tach4() {
   phase4 = 0;
 }
 
-void drive(int pin, int duty, int freq) {
-  /*
-  int dtpwm = 1e6/freq; // in microseconds
-  int highdutydt = pwm*dtpwm/200;
-  int lowdutydt = dtpwm - highdutydt;
-  Serial.println(t-tpwm);
-
-  if ((t-tpwm) >= highdutydt){
-    digitalWrite(pin,HIGH);
-    tpwm = t;
-  }
-  else{
-    digitalWrite(pin,LOW);
-  }
-  
-
-  TIM_TypeDef *Instance4 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(pin), PinMap_PWM);
-  uint32_t channel4 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(pin), PinMap_PWM));
-  HardwareTimer *MyTim4 = new HardwareTimer(Instance4);
-  MyTim4->setPWM(channel4, pin, freq, duty); 
-  */
+void drive(int pin, int duty, int freq) { // duty is really pwm and is an artifact; pwm ranges from 0-65000 (16 bit) on the nucleo
   analogWrite(pin,duty);
   analogWriteFrequency(freq);
 }
 
+void startMotors(){
+  drive(motor2pin,startuppwm, pwmfreq);
+  drive(motor4pin,startuppwm, pwmfreq);
+  delay(1000);
+  drive(motor2pin,startuppwm, pwmfreq);
+  drive(motor4pin,startuppwm, pwmfreq);
+  delay(4000);
+}
+
+int XOR(int x, int y){
+  if (x == 1 && y == 1){
+    return 0;
+  }
+  else if (x == 1 || y == 1){
+    return 1;
+  }
+  return 0;
+}
